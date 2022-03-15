0ae43a86cb228f16b548244e6458b2a5fe6e8bfb
Bruno Garcia Moura <bgarciamoura@gmail.com>
2022-03-15 20:31:50 -0300

    feat: Frontend environment builded

---
+++ frontend/src/pages/Home/index.tsx
@@ -0,0 +1,11 @@
+import React, { useEffect, useState } from 'react';
+
+const Home: React.FC = () => {
+    return (
+        <div>
+            <p>Hello from Home</p>
+        </div>
+    );
+};
+
+export default Home;
